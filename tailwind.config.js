d4562867c5d0a8f169a6dd2e911ffdbb6d1e64c9
Dahl Tamares <dahltamares89@gmail.com>
2023-01-19 22:52:34 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        extend: {
+            colors: {
+                border: "#e8dfec",
+                nav: "#fdf9ff",
+                home: "#f2f2fc",
+                textTitle: "#302e4d",
+                textLess: "#504e70",
+                textSpan: "#970017",
+            },
+        },
+    },
+    plugins: [],
+};
